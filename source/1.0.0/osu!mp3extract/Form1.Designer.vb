3346be4c443edd98122abbadf0793650d49a0544
Mxater <Mxater@gmail.com>
2013-06-06 10:00:56 -0400

    Source 1.0.0
    Visual Studio 2010

---
+++ source/1.0.0/osu!mp3extract/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Carpeta ""Songs"" de osu!"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(15, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(205, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Examinar..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Carpeta salida"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.Location = New System.Drawing.Point(15, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(205, 61)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(70, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Examinar..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(15, 90)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(257, 30)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Comenzar!"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 126)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(257, 31)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 168)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "osu!mp3extract"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+
+End Class
---
+++ source/1.0.0/osu!mp3extract/Form1.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.Reset()
+        Dim r = FolderBrowserDialog1.ShowDialog()
+        If r = Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        FolderBrowserDialog1.Reset()
+        Dim r = FolderBrowserDialog1.ShowDialog()
+        If r = Windows.Forms.DialogResult.OK Then
+            TextBox2.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub TextBox1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBox1.Click
+        Button1.PerformClick()
+    End Sub
+
+    Private Sub TextBox2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBox2.Click
+        Button2.PerformClick()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim osufolder = TextBox1.Text.ToString
+        Dim tofolder = TextBox2.Text.ToString
+        Dim songsok As New ArrayList
+        If TextBox1.Text.Length = 0 Then
+            MsgBox("Debe seleccionar la carpeta songs de osu!", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly)
+            Exit Sub
+        End If
+        If TextBox2.Text.Length = 0 Then
+            MsgBox("Debe seleccionar la carpeta de salida de los mp3", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly)
+            Exit Sub
+        End If
+        If Not My.Computer.FileSystem.DirectoryExists(osufolder) Then
+            MsgBox("La carpeta seleccionada no existe!", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly)
+            Exit Sub
+        End If
+        If Not My.Computer.FileSystem.DirectoryExists(tofolder) Then
+            My.Computer.FileSystem.CreateDirectory(tofolder)
+        End If
+
+        My.Settings.osufolder = osufolder
+        My.Settings.tofolder = tofolder
+        My.Settings.Save()
+        Dim songsfolders = My.Computer.FileSystem.GetDirectories(osufolder)
+        ProgressBar1.Maximum = songsfolders.Count
+        For Each folder In songsfolders
+            Dim files = My.Computer.FileSystem.GetFiles(folder)
+            Dim listfileok As New ArrayList
+            For Each file In files
+                If file.ToLower.EndsWith(".osu") Then
+                    Dim fileosu As New System.IO.StreamReader(file)
+                    Dim ends = False
+                    Dim songartist = ""
+                    Dim songtitle = ""
+                    Dim audiofile = ""
+                    While ends = False
+                        If fileosu.EndOfStream Then Exit While
+                        Dim linea = fileosu.ReadLine
+
+                        If linea.StartsWith("AudioFilename: ") Then
+                            audiofile = linea.Remove(0, "AudioFilename: ".Length)
+                        End If
+                        If linea.StartsWith("Title:") Then
+                            songtitle = linea.Remove(0, "Title:".Length)
+                        End If
+                        If linea.StartsWith("Artist:") Then
+                            songartist = linea.Remove(0, "Artist:".Length)
+                        End If
+                    End While
+                    fileosu.Close()
+
+                    audiofile = Trim(audiofile)
+                    songtitle = Trim(songtitle)
+                    songartist = Trim(songartist)
+
+                    If audiofile = "" Then Continue For
+                    If listfileok.Contains(audiofile) Then Continue For
+
+                    Dim newfilename As String = ""
+                    If Not songartist = "" And Not songtitle = "" Then
+                        newfilename = songartist & " - " & songtitle & audiofile.Substring(audiofile.LastIndexOf("."), audiofile.Length - audiofile.LastIndexOf("."))
+                    Else
+                        newfilename = audiofile
+                    End If
+                    listfileok.Add(audiofile)
+
+                    Dim reg As New System.Text.RegularExpressions.Regex("[^a-zA-Z0-9\(\) \.\-_\^\!\[\]]")
+                    newfilename = reg.Replace(newfilename, "")
+
+                    'If My.Computer.FileSystem.FileExists(tofolder & "\" & newfilename) Then
+                    'Continue For
+                    'End If
+
+                    Try
+                        System.IO.File.Copy(folder & "\" & audiofile, tofolder & "\" & newfilename, True)
+                    Catch ex As Exception
+
+                    End Try
+
+
+                    ' Dim prop As New DSOFile.OleDocumentProperties
+                    'prop.Open(tofolder & "\" & newfilename)
+
+                    If Not audiofile.Substring(audiofile.LastIndexOf("."), audiofile.Length - audiofile.LastIndexOf(".")).ToLower = ".mp3" Then
+                        Continue For
+                    End If
+                    Dim mp3 As New MP3ID3v1(tofolder & "\" & newfilename)
+                    If (mp3.TagExists) Then
+                        Dim dirty As Boolean = False
+                        If mp3.Frame(MP3ID3v1.FrameTypes.Title) = "" Then
+                            mp3.Frame(MP3ID3v1.FrameTypes.Title) = songtitle
+                            dirty = True
+                        End If
+
+                        If mp3.Frame(MP3ID3v1.FrameTypes.Artist) = "" Then
+                            mp3.Frame(MP3ID3v1.FrameTypes.Title) = songartist
+                            dirty = True
+                        End If
+
+                        If dirty Then mp3.Update()
+                    Else
+                        mp3.Frame(MP3ID3v1.FrameTypes.Title) = songtitle
+                        mp3.Frame(MP3ID3v1.FrameTypes.Artist) = songartist
+                        mp3.Frame(MP3ID3v1.FrameTypes.Track) = 0
+                        mp3.Frame(MP3ID3v1.FrameTypes.Genre) = MP3ID3v1.Genres.Other
+                        mp3.Update()
+                    End If
+                End If
+            Next
+            ProgressBar1.Value += 1
+        Next
+        MsgBox("Listo", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = My.Settings.osufolder
+        TextBox2.Text = My.Settings.tofolder
+    End Sub
+End Class
